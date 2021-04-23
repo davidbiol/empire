0297e7d92228e7cd743358e6538d0ac16c5eaa42
David Gutierrez-Duque <davidgd2015@gmail.com>
2021-04-23 18:25:01 -0500

    Parameter name added to function hello.R

--- R/hello.R
+++ R/hello.R
@@ -13,6 +13,6 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+hello <- function(name) {
+  print("Hello world!, my name is" + name)
 }
