9696b60ea53f3bfb0b4f990ef6fe24b36ec24f96
David Gutierrez-Duque <davidgd2015@gmail.com>
2021-04-27 12:18:29 -0500

    gen_sim_norm function created

--- validation_tests/data/data_generator/sim_corr.R
+++ validation_tests/data/data_generator/sim_corr.R
@@ -1,18 +1,15 @@
-
 # Simular datos con variables correlacionadas --------------------------
-
 library(faux)
-
-dat <- rnorm_multi(n = 100, 
+dat <- rnorm_multi(n = 100,
                    mu = c(20, 20, 20),
                    sd = c(1, 5, 5),
-                   r = c(0.5, 0.6, 0.99), 
+                   r = c(0.5, 0.6, 0.99),
                    varnames = c("A", "B", "C"),
                    empirical = TRUE)
 
 #Empirical=TRUE es para que los datos sean la poblacion, es decir, el promedio, la sd y el r son exactos.
 
-
+cor(dat)
 # Para saber si los r son permitidos: -------------------------------------
 
 r = c(.2, .3, .4, .2,
