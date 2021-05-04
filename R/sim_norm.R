85a584a56991e38f26d7f4cfd19b2fd4a8178ef9
David Gutierrez-Duque <davidgd2015@gmail.com>
2021-05-04 17:37:17 -0500

    multivariate normality tests on sim_norm

--- R/sim_norm.R
+++ R/sim_norm.R
@@ -16,3 +16,24 @@ sim_norm <- function(n, k=3, mean.v=rep(0,k), sd.v=rep(1,k), lambda, normality=T
   }
 }
 
+data_ej <- sim_norm(100, 4, mean.v=c(10,3,20,2), sd.v=c(0.1,0.2,0.14,0.01))
+
+data_ej_no_norm <- sim_norm(100, 4, lambda=3, normality=FALSE)
+#Multivariate normality test
+library(MVN)
+
+#Mardia’s multivariate skewness and kurtosis coefficients, if both tests indicates multivariate normality, then data follows a multivariate normality distribution at the 0.05 significance level
+mvn(data_ej, mvnTest = "mardia")
+mvn(data_ej_no_norm, mvnTest = "mardia")
+
+#Royston test, it depends on shapiro-wilk tests
+mvn(data_ej, mvnTest = "royston")
+mvn(data_ej_no_norm, mvnTest = "royston")
+
+#Doornik-Hansen’s MVN test
+mvn(data_ej, mvnTest = "dh")
+mvn(data_ej_no_norm, mvnTest = "dh")
+
+#Energy test
+mvn(data_ej, mvnTest = "energy")
+mvn(data_ej_no_norm, mvnTest = "energy")
