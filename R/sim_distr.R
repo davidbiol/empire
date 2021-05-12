0511b69f5bc1614fb75dd3d4ecf66ca71c95f642
David Gutierrez-Duque <davidgd2015@gmail.com>
2021-05-12 15:55:34 -0500

    chisquare, gamma and uniform distributions added to sim_distr

--- R/sim_distr.R
+++ R/sim_distr.R
@@ -3,8 +3,8 @@
 #'
 #' @param n number of observations.
 #' @param k number of variables.
-#' @param distr the distribution to be used. This must be one of "normal", "exponential", "poisson" or "weibull".
-#' @param ... arguments specific to each distribution. See \code{\link[stats:rnorm]{rnorm}}, \code{\link[stats:rexp]{rexp}}, \code{\link[stats:rpois]{rpois}}, \code{\link[stats:rweibull]{rweibull}} to know these arguments.
+#' @param distr the distribution to be used. This must be one of "normal", "exponential", "poisson", "weibull", "chisquare", "gamma" or "uniform".
+#' @param ... arguments specific to each distribution. See \code{\link[stats:rnorm]{rnorm}}, \code{\link[stats:rexp]{rexp}}, \code{\link[stats:rpois]{rpois}}, \code{\link[stats:rweibull]{rweibull}}, \code{\link[stats:rchisq]{rchisq}}, \code{\link[stats:rgamma]{rgamma}}, \code{\link[stats:runif]{runif}} to know these arguments.
 #'
 #' @return
 #' \code{sim_distr} gives a data frame with the observations as rows and the variables as columns
@@ -12,13 +12,15 @@
 #'
 #' @examples
 #'
-#' @seealso \code{\link[stats:rexp]{rexp}},
-#' \code{\link[stats:rnorm]{rnorm}},
+#' @seealso \code{\link[stats:rnorm]{rnorm}},
 #' \code{\link[stats:rexp]{rexp}},
 #' \code{\link[stats:rpois]{rpois}},
-#' \code{\link[stats:rweibull]{rweibull}}.
+#' \code{\link[stats:rweibull]{rweibull}},
+#' \code{\link[stats:rchisq]{rchisq}},
+#' \code{\link[stats:rgamma]{rgamma}},
+#' \code{\link[stats:runif]{runif}}.
 
-sim_distr <- function(n, k=3, distr, ...){
+sim_distr <- function(n, k = 3, distr, ...){
 
   df <- data.frame(matrix(ncol=k, nrow=n))
   rdistr <- switch(distr,
@@ -26,6 +28,9 @@ sim_distr <- function(n, k=3, distr, ...){
          "exponential" = stats::rexp(n, ...),
          "poisson" = stats::rpois(n, ...),
          "weibull" = stats::rweibull(n, ...),
+         "chisquare" = stats::rchisq(n, ...),
+         "gamma" = stats::rgamma(n, ...),
+         "uniform" = stats::runif(n, ...),
          stop("Distribution unknown, try with a known distribution. See ?sim_norm 'distr' for help"))
 
   for(i in seq_len(k)) {
