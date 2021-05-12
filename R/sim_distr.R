3e5e3bad279b95b010f8527b75792d418fd93986
David Gutierrez-Duque <davidgd2015@gmail.com>
2021-05-12 15:33:28 -0500

    links and better arguments added to sim_distr documentation

--- R/sim_distr.R
+++ R/sim_distr.R
@@ -1,26 +1,31 @@
 
 #' @title Simulate data variables with known distributions
 #'
-#'
-#' @param n number of observations
-#' @param k number of variables
-#' @param distr
-#' @param ...
+#' @param n number of observations.
+#' @param k number of variables.
+#' @param distr the distribution to be used. This must be one of "normal", "exponential", "poisson" or "weibull".
+#' @param ... arguments specific to each distribution. See \code{\link[stats:rnorm]{rnorm}}, \code{\link[stats:rexp]{rexp}}, \code{\link[stats:rpois]{rpois}}, \code{\link[stats:rweibull]{rweibull}} to know these arguments.
 #'
 #' @return
-#' sim_distr gives a data frame with the observations as rows and the variables as columns
+#' \code{sim_distr} gives a data frame with the observations as rows and the variables as columns
 #' @export
 #'
 #' @examples
+#'
+#' @seealso \code{\link[stats:rexp]{rexp}},
+#' \code{\link[stats:rnorm]{rnorm}},
+#' \code{\link[stats:rexp]{rexp}},
+#' \code{\link[stats:rpois]{rpois}},
+#' \code{\link[stats:rweibull]{rweibull}}.
 
 sim_distr <- function(n, k=3, distr, ...){
 
   df <- data.frame(matrix(ncol=k, nrow=n))
   rdistr <- switch(distr,
-         "Normal" = stats::rnorm(n, ...),
-         "Exponential" = stats::rexp(n, ...),
-         "Poisson" = stats::rpois(n, ...),
-         "Weibull" = stats::rweibull(n, ...),
+         "normal" = stats::rnorm(n, ...),
+         "exponential" = stats::rexp(n, ...),
+         "poisson" = stats::rpois(n, ...),
+         "weibull" = stats::rweibull(n, ...),
          stop("Distribution unknown, try with a known distribution. See ?sim_norm 'distr' for help"))
 
   for(i in seq_len(k)) {
