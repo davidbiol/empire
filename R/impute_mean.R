49fa1cfd979718ba0bf4bd8cc4afb891204a5c90
David Gutierrez-Duque <davidgd2015@gmail.com>
2021-05-24 17:14:38 -0500

    description parameters added

--- R/impute_mean.R
+++ R/impute_mean.R
@@ -1,13 +1,16 @@
 #' Simple mean imputation of missing values
 #'
-#' @param data
+#' @param data A matrix or data frame object. It should have variables as columns and observations as rows.
 #'
-#' @return
+#' @return A list object with positions of missing values (positions), estimated values (est_values) and new data with imputed/estimated values (new_data)
 #' @export
 #'
 #' @examples
 impute_mean <- function(data){
-
+  # Exceptions
+  if (!is.data.frame(data) & !is.matrix(data)){
+    stop("Data to be evaluated (data) should be a data frame or a matrix")
+  }
   positions <- pos_miss(data)
 
   est_values = vector()
@@ -21,4 +24,5 @@ for(i in seq_len(nrow(positions))){
   # List
   my_list <- list("positions" = positions, "est_values" = est_values, "new_data" = new_data)
 
+  return(my_list)
 }
