e86557c388cbde93978238ac8e168b33ca72dcf3
David Gutierrez-Duque <davidgd2015@gmail.com>
2021-05-24 20:33:20 -0500

    param and return documentation added in impute_median

--- R/impute_median.R
+++ R/impute_median.R
@@ -1,23 +1,23 @@
 #' Simple median imputation of missing values
 #'
-#' @param data
+#' @param data A matrix or data frame object. It should have variables as columns and observations as rows.
 #'
-#' @return
+#' @return A list object with positions of missing values (positions), imputed values by observed median (imp_values) and new data with imputed values (new_data).
 #' @export
 #'
 #' @examples
 impute_median <- function(data){
   positions <- pos_miss(data)
 
-  est_values = vector()
+  imp_values = vector()
   new_data = data
 
   for(i in seq_len(nrow(positions))){
     new_data[positions[i,1], positions[i,2]] <- stats::median(data[, positions[i,2]], na.rm=TRUE)
-    est_values[i] <- new_data[positions[i,1], positions[i,2]]
+    imp_values[i] <- new_data[positions[i,1], positions[i,2]]
   }
 
   # List
-  my_list <- list("positions" = positions, "est_values" = est_values, "new_data" = new_data)
+  my_list <- list("positions" = positions, "imp_values" = imp_values, "new_data" = new_data)
 
 }
