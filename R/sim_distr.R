a2921176ebd4bb2da66e0959a79195d913e7c7c1
David Gutierrez-Duque <davidgd2015@gmail.com>
2021-05-26 15:10:11 -0500

    the case with just one distribution improved for many variable arguments

--- R/sim_distr.R
+++ R/sim_distr.R
@@ -33,32 +33,47 @@ sim_distr <- function(n, k, distr, ...){
     distr=rep(distr, k)
   }
 
-  tryCatch(if(length(distr)!=k) stop("The number of distributions ('distr') must be the same number of variables ('k').", call.=FALSE))
-
   df <- data.frame(matrix(ncol=k, nrow=n))
 
   # Assign arguments to each distribution
   distr_args <- list(...)
-  normal_arg <- c(distr_args$mean, distr_args$sd) #assign mean=distr_args$mean...
-  exp_arg <- c(distr_args$rate)
-  poisson_arg <- c(distr_args$lambda)
-  weibull_arg <- c(distr_args$shape, distr_args$scale)
-  chisquare_arg <- c(distr_args$df, distr_args$ncp)
-  gamma_arg <- c(distr_args$shape, distr_args$rate, distr_args$scale) #It can have issues
-  unif_arg <- c(min=distr_args$min, max=distr_args$max)
+  normal_arg <- data.frame(mean=distr_args$mean, sd=distr_args$sd)
+  exp_arg <- data.frame(rate=distr_args$rate)
+  poisson_arg <- data.frame(lambda=distr_args$lambda)
+  weibull_arg <- data.frame(shape=distr_args$shape, scale=distr_args$scale)
+  chisquare_arg <- data.frame(df=distr_args$df, ncp=distr_args$ncp)
+  # gamma_arg <- data.frame(shape=distr_args$shape, rate=distr_args$rate, scale=distr_args$scale)
+  unif_arg <- data.frame(min=distr_args$min, max=distr_args$max)
 
   for(i in seq_len(k)) {
     tryCatch(rlang::arg_match0(distr[i], c("normal", "exponential", "poisson", "weibull", "chisquare", "gamma", "uniform"), arg_nm = substitute("Distribution elements")))
 
+  if (length(table(distr))==1){ #The case with just one distribution
     rdistr <- switch(distr[i],
-                     "normal" = do.call(stats::rnorm, c(list(n), normal_arg)),
-                     "exponential" = do.call(stats::rexp, c(list(n), exp_arg)),
-                     "poisson" = do.call(stats::rpois, c(list(n), poisson_arg)),
-                     "weibull" = do.call(stats::rweibull, c(list(n), weibull_arg)),
-                     "chisquare" = do.call(stats::rchisq, c(list(n), chisquare_arg)),
-                     "gamma" = do.call(stats::rgamma, c(list(n), gamma_arg)),
-                     "uniform" = do.call(stats::runif, c(list(n), unif_arg))) #It's not working
+                     "normal" = do.call(stats::rnorm, c(list(n), normal_arg[i,])),
+                     "exponential" = do.call(stats::rexp, c(list(n), exp_arg[i,])),
+                     "poisson" = do.call(stats::rpois, c(list(n), poisson_arg[i,])),
+                     "weibull" = do.call(stats::rweibull, c(list(n), weibull_arg[i,])),
+                     "chisquare" = do.call(stats::rchisq, c(list(n), chisquare_arg[i,])),
+                     "gamma" = do.call(stats::rgamma, c(list(n), gamma_arg[i,])),
+                     "uniform" = do.call(stats::runif, c(list(n), unif_arg[i,])))
+  }
+    else {
+      rdistr <- switch(distr[i],
+                       "normal" = do.call(stats::rnorm, c(list(n), normal_arg)),
+                       "exponential" = do.call(stats::rexp, c(list(n), exp_arg)),
+                       "poisson" = do.call(stats::rpois, c(list(n), poisson_arg)),
+                       "weibull" = do.call(stats::rweibull, c(list(n), weibull_arg)),
+                       "chisquare" = do.call(stats::rchisq, c(list(n), chisquare_arg)),
+                       "gamma" = do.call(stats::rgamma, c(list(n), gamma_arg)),
+                       "uniform" = do.call(stats::runif, c(list(n), unif_arg)))
+
+    }
+    #Save distribution in data frame
     rdistr -> df[i]
+
+    tryCatch(if(length(distr)!=k) stop("The number of distributions ('distr') must be the same number of variables ('k').", call.=FALSE))
+
   }
   return(df)
 
