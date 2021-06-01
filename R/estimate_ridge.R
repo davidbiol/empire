ff703cc6c8e46cdec2008d7dcd1beeb8cb3a7ea6
David Gutierrez-Duque <davidgd2015@gmail.com>
2021-06-01 15:00:52 -0500

    omit NA in the same row where the estimation occurs

--- R/estimate_ridge.R
+++ R/estimate_ridge.R
@@ -1,27 +1,26 @@
-#' Estimate missing values using multiple linear regressions
+#' Estimate missing values using Ridge regression
 #'
 #' @param data A matrix or data frame object. It should have variables as columns and observations as rows.
 #'
-#' @return A list object with positions of missing values (positions), estimated values by multiple linear regression (est_values) and new data with estimated values (new_data).
+#' @return A list object with positions of missing values (positions), estimated values by multiple linear regression with Ridge penalization (est_values) and new data with estimated values (new_data).
 #' @export
 #'
 #' @examples
-estimate_mlr <- function(data){
+estimate_ridge <- function(data){
 
   positions <- pos_miss(data)
 
-  estimate.mv <- function(v){
+  ridge.reg <- function(v){
     complete_data <- data[stats::complete.cases(data),]
     Y = as.matrix(complete_data[positions[v,2]])
-    if (length(data[positions[v,1],][is.na(data[positions[v,1],])])!=1){
-      X = as.matrix(cbind(c(rep(1, nrow(Y))), c.t[-c(positions[v,2], c(which(is.na(data[positions[v,1],]))))]))
-    }
-    else {
-      X = as.matrix(cbind(c(rep(1, nrow(Y))), c.t[-(positions[v,2])]))
-    }
+    X = as.matrix(cbind(c(rep(1, nrow(Y))), complete_data[-(positions[v,2])]))
     XtX <- t(X)%*%X
+
+    #Ridge penalization
+    cv.out=cv.glmnet(X,Y,alpha=0)
+    bestlam=cv.out$lambda.min
     XtX.1 <- solve(XtX)
-    B <- XtX.1%*%t(X)%*%Y # Calculate coefficients
+    B <- solve(XtX + bestlam*diag(1,nrow(XtX)))%*%t(X)%*%Y #Calculate coefficients with Ridge Lambda penalization
 
     regresion <- function(b1,b2){   # Regression function
       B[b1]*data[positions[v,1],b2]
@@ -30,8 +29,8 @@ estimate_mlr <- function(data){
 
     e1 <- B[1] # Value obtained with the regression
     b=2
-    for (j in seq_len(ncol(data))){ # Omit the value of the target column, replacing in all columns except where the missing value lies
-      if (all(j != c(positions[v,2],c(which(is.na(data[positions[v,1],])))))){ # Check all False
+    for (j in seq_len(ncol(data))){ # Omit the value in the target column, replacing in all columns except where the missing value lies
+      if (j != positions[v,2]){
         e1 = e1 + regresion(b,j)
         b=b+1
       }
