47d401e2afc2080670453bfef74c9f675333d427
David Gutierrez-Duque <davidgd2015@gmail.com>
2021-07-07 21:06:28 -0500

    Modularization of Ridge function with mlr function

--- R/estimate_ridge.R
+++ R/estimate_ridge.R
@@ -8,39 +8,11 @@
 #' @examples
 estimate_ridge <- function(data){
 
-  positions <- pos_miss(data)
+  positions <- empire::pos_miss(data)
 
-  estimate.mv <- function(v){
-    complete_data <- data[stats::complete.cases(data),]
-    Y = as.matrix(complete_data[positions[v,2]])
-    if (length(data[positions[v,1],][is.na(data[positions[v,1],])])!=1){
-      X = as.matrix(cbind(c(rep(1, nrow(Y))), complete_data[-c(positions[v,2], c(which(is.na(data[positions[v,1],]))))]))
-    }
-    else {
-      X = as.matrix(cbind(c(rep(1, nrow(Y))), complete_data[-(positions[v,2])]))
-    }
-    XtX <- t(X)%*%X
-    XtX.1 <- solve(XtX)
-    B <- XtX.1%*%t(X)%*%Y # Calculate coefficients
-
-    regresion <- function(b1,b2){   # Regression function
-      B[b1]*data[positions[v,1],b2]
-    }
-
-
-    e1 <- B[1] # Value obtained with the regression
-    b=2
-    for (j in seq_len(ncol(data))){ # Omit the value of the target column, replacing in all columns except where the missing value lies
-      if (all(j != c(positions[v,2],c(which(is.na(data[positions[v,1],])))))){ # Check all False
-        e1 = e1 + regresion(b,j)
-        b=b+1
-      }
-    }
-
-    # Return e1
-    return(e1)
-
-  }
+  #Apply Simple Multiple Linear Regression
+  mlr_est = empire::estimate_mlr(data)
+  data = mlr_est$new_data
 
   ridge.reg <- function(v){
     complete_data <- data[stats::complete.cases(data),]
@@ -79,48 +51,6 @@ estimate_ridge <- function(data){
 
   }
 
-  # Iterate the estimators
-  repeat{
-    # old estimated values
-    old_est_values = vector()
-    g = 1
-    # Run ridge.reg in each missing value
-    for (mv in seq_len(nrow(positions))){
-      old_est_values[g] <- estimate.mv(mv)
-      g=g+1
-    }
-    print(old_est_values)
-
-    for(i in seq_len(nrow(positions))){
-      data[positions[i,1], positions[i,2]] <- old_est_values[i]
-    }
-
-
-    # new estimated values
-    new_est_values = vector()
-    g=1
-
-    for (mv in seq_len(nrow(positions))){
-      new_est_values[g] <- estimate.mv(mv)
-      g=g+1
-    }
-
-    print(new_est_values)
-    # Catch error, na's in estimation
-    tryCatch(if(any(is.na(new_est_values))) stop("na's in estimation"))
-
-    for(i in seq_len(nrow(positions))){
-      data[positions[i,1], positions[i,2]] <- new_est_values[i]
-    }
-
-    # Exit test
-    conv <- old_est_values - new_est_values
-    print(conv)
-    ifelse(all((lapply(conv, abs) < 1e-5)==TRUE), break, next)
-
-
-
-  }
   #Apply Ridge
   ridge_est_values <- vector()
   g=1
