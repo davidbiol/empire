8df2f8857d3ce30c8abad018f7ce278e90d05350
David Gutierrez-Duque <davidgd2015@gmail.com>
2021-09-06 11:13:42 -0500

    update exceptions in count_miss

--- R/count_miss.R
+++ R/count_miss.R
@@ -9,11 +9,13 @@
 
 count_miss <- function(data){
   # Exceptions
-  if (!is.data.frame(data) & !is.matrix(data) & !is.vector(data)){
-    stop("Data to be evaluated (data) should be a data frame or a matrix")
+
+  if (!is.data.frame(data) & !is.matrix(data)){
+    stop("Data to be evaluated (data) should be a data frame or a matrix. If it's in fact a vector, try 'which(is.na(data))")
   }
-  if (is.vector(data)){
-    warning("Data to be evaluated is a vector and it should be a data frame or a matrix")
+
+  if(!all(unlist(lapply(data, is.numeric)), TRUE)){
+    stop("Data to be evaluated has a non-numeric element. Chack as.numeric() to convert your data to a numeric class")
   }
 
   return(length(which(is.na(data))))
