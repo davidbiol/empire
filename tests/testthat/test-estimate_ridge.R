3503311121b3734b0983bbdc321318a73b5f98bd
David Gutierrez-Duque <davidgd2015@gmail.com>
2021-09-06 10:28:08 -0500

    test values are accurately estimated in estimate_ridge

--- tests/testthat/test-estimate_ridge.R
+++ tests/testthat/test-estimate_ridge.R
@@ -1,3 +1,13 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+## data
+x1 <- c(1.30, 1.40, NA, 1.54, 1.30, NA, 1.50, 1.60, 1.41, 1.34)
+x2 <- c(51, 60, 69, 73, 56, 75, 80, NA, 58, 70)
+x3 <- c(4.8, NA, 5.8, 6.5, 5.3, 7.0, 8.1, 7.8, 5.9, 6.1)
+x4 <- c(115, 130, 138, 148, 122, 152, 160, 155, 135, 140)
+df <- data.frame(x1,x2,x3,x4)
+
+## Check it is accurately estimating
+
+test_that("Values are accurately estimated", {
+  object_ridge <- estimate_ridge(df)
+  expect_equal(obj_ridge$est_values, c(1.386813, 1.504242, 74.948649, 5.739797))
 })
